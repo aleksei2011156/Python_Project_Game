2481572363ebdeb3811ab8e4b491c0abfab433b9
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-08-12 23:58:11 +0400

    Start project game.

---
+++ settings.py
@@ -0,0 +1,8 @@
+"""" Класс для хранения всех настройек игры."""
+class Settings():
+    def __init__(self):
+        """Инициализирует найтроки игры."""
+        # Параметр экрана.
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color  = (230, 230, 230)
