0c80cee1516ad225e3d40f3452e6687b3fedc1ff
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-08-13 15:23:56 +0400

    Refactoring: Helper Method.

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,18 +26,22 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-                # Отслеживание событий клавиатуры и мыши.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
+                self._check_events()
+                self._update_screen()
 
-                # При каждом проходе цикла перерисовывается экран.
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
+    def _check_events(self):
+        """Обрабатывает нажатие клавиш и мыши."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-                # Отслеживание последнего прорисованного экрана.
-                pygame.display.flip()
+    def _update_screen(self):
+        """Обновляет изображение на экране и отоюбражает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
+        # Отслеживание последнего прорисованного экрана.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
