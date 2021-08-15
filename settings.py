48802ba9b299e8088b565d95b35c93678acb0c7e
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-08-15 23:25:23 +0400

    Shooting

--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ class Settings():
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color  = (230, 230, 230)
-        #Настройка корабля
+        # Настройка корабля
         self.ship_speed = 1.5
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
 
