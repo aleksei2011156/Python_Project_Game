9444d67cd7eb63c2a3e7905064074f1e0c234ef3
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-10-11 11:02:08 +0300

    Final Fleet Alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,9 +32,9 @@ class AlienInvasion:
         while True:
                 self._check_events()
                 self.ship.update()
-                self._update_screen()
                 self._update_bullets()
-
+                self._update_aliens()
+                self._update_screen()
 
     def _check_events(self):
         """Обрабатывает нажатие клавиш и мыши."""
@@ -80,6 +80,13 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """
+        Проверяет, достиг ли флот края экрана, с последующим обновлением позиций всех пришельцев во флоте.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         """ Создание флота вторжения. """
         # Создание пришельца и вычисление количиства пришельцев в ряду.
@@ -107,6 +114,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + (2 * alien.rect.height * row_number)
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Обновляет изображение на экране и отоюбражает новый экран."""
--- settings.py
+++ settings.py
@@ -14,5 +14,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
-
+        # Настройка пришельцев
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение в право; а -1 - влево.
+        self.fleet_direction = 1
 
