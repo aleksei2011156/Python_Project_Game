89820910a8a8d9367597db674e383c44883200cb
aleksei2011156 <alekseipushkarskii97@gmail.com>
2021-10-13 00:09:05 +0300

    Button Start 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,6 +65,8 @@ class AlienInvasion:
         """Запускает новую игру при нажатии кнопки Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pose)
         if button_clicked and not self.stats.game_active:
+            # Сброс игровых настроек.
+            self.settings.initialize_dynamic_settings()
             # Сброс игровой статистики.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -126,6 +128,7 @@ class AlienInvasion:
             # Уничтожение существующих снарядов  и создание нового флота.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """
--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 """" Класс для хранения всех настройек игры."""
 class Settings():
     def __init__(self):
-        """Инициализирует найтроки игры."""
+        """Инициализирует статические найтроки игры."""
         # Параметр экрана.
         self.screen_width = 1920
         self.screen_height = 1080
@@ -10,15 +10,37 @@ class Settings():
         self.ship_speed = 1.5
         self.ship_limit = 3
         # Параметры снаряда
-        self.bullet_speed = 2
-        self.bullet_width = 3
+        self.bullet_speed = 3
+        self.bullet_width = 80
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
 
         # Настройка пришельцев
-        self.alien_speed = 0.7
+        # self.alien_speed = 0.7
         self.fleet_drop_speed = 7
+
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходу игры."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3.0
+        self.alien_speed_factor = 1.0
+
         # fleet_direction = 1 обозначает движение в право; а -1 - влево.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+
+
 
